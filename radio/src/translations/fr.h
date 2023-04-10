089a0c7d9d4edbaf6803a206e172b1d691292940
xy2019 <xy_3202@163.com>
2023-04-10 17:55:00 +0800

    add language
    # Conflicts:
    #	radio/src/translations/fr.h

--- radio/src/translations/fr.h
+++ radio/src/translations/fr.h
@@ -1301,4 +1301,10 @@
 #define TR_USBJOYSTICK_AXIS_COLLISION  "!Axis collision!"
 #define TR_USBJOYSTICK_CIRC_COUTOUT    TR("Circ. cut", "Circular cutout")
 #define TR_VUSBJOYSTICK_CIRC_COUTOUT   "None","X-Y, Z-rX","X-Y, rX-rY"
-#define TR_USBJOYSTICK_APPLY_CHANGES   "Apply changes"
+#define TR_USBJOYSTICK_APPLY_CHANGES   "Appliquer changements"
+
+#define TR_DIGITAL_SERVO          "Servo333HZ"
+#define TR_ANALOG_SERVO           "Servo 50HZ"
+#define TR_SIGNAL_OUTPUT          "Signal output"
+#define TR_SERIAL_BUS             "Serial bus"
+#define TR_SYNC                   "Sync"
