3b759b2c91311f1ff7bbd3b3299e7c6c1ad7a1f6
Richard Li <richard.li@ces.hk>
2023-04-29 12:23:10 +0800

    Fixed garbage label.

--- radio/src/VirtualFS.cpp
+++ radio/src/VirtualFS.cpp
@@ -633,129 +633,15 @@ VfsError VfsFile::putc(char c)
 
 int VfsFile::fprintf(const char* str, ...)
 {
-  switch(type)
-  {
-#if defined (USE_FATFS)
-  case VfsFileType::FAT:
-  {
-      va_list args;
-      va_start(args, str);
+  char line[256];
+  va_list args;
+  va_start(args, str);
+  vsprintf(line, str, args);
+  va_end(args);
 
-      int ret = f_printf(&fat.file, str, args);
-      va_end(args);
-      return ret;
-  }
-#endif
-#if defined(USE_LITTLEFS)
-  case VfsFileType::LFS:
-  {
-#if 0
-      va_list arp;
-         putbuff pb;
-         BYTE f, r;
-         UINT i, j, w;
-         DWORD v;
-         TCHAR c, d, str[32], *p;
-
-
-         putc_init(&pb, fp);
-
-         va_start(arp, fmt);
-
-         for (;;) {
-             c = *fmt++;
-             if (c == 0) break;          /* End of string */
-             if (c != '%') {             /* Non escape character */
-                 putc_bfd(&pb, c);
-                 continue;
-             }
-             w = f = 0;
-             c = *fmt++;
-             if (c == '0') {             /* Flag: '0' padding */
-                 f = 1; c = *fmt++;
-             } else {
-                 if (c == '-') {         /* Flag: left justified */
-                     f = 2; c = *fmt++;
-                 }
-             }
-             if (c == '*') {             /* Minimum width by argument */
-                 w = va_arg(arp, int);
-                 c = *fmt++;
-             } else {
-                 while (IsDigit(c)) {    /* Minimum width */
-                     w = w * 10 + c - '0';
-                     c = *fmt++;
-                 }
-             }
-             if (c == 'l' || c == 'L') { /* Type prefix: Size is long int */
-                 f |= 4; c = *fmt++;
-             }
-             if (c == 0) break;
-             d = c;
-             if (IsLower(d)) d -= 0x20;
-             switch (d) {                /* Atgument type is... */
-             case 'S' :                  /* String */
-                 p = va_arg(arp, TCHAR*);
-                 for (j = 0; p[j]; j++) ;
-                 if (!(f & 2)) {                     /* Right padded */
-                     while (j++ < w) putc_bfd(&pb, ' ') ;
-                 }
-                 while (*p) putc_bfd(&pb, *p++) ;        /* String body */
-                 while (j++ < w) putc_bfd(&pb, ' ') ;    /* Left padded */
-                 continue;
-
-             case 'C' :                  /* Character */
-                 putc_bfd(&pb, (TCHAR)va_arg(arp, int)); continue;
-
-             case 'B' :                  /* Unsigned binary */
-                 r = 2; break;
-
-             case 'O' :                  /* Unsigned octal */
-                 r = 8; break;
-
-             case 'D' :                  /* Signed decimal */
-             case 'U' :                  /* Unsigned decimal */
-                 r = 10; break;
-
-             case 'X' :                  /* Unsigned hexdecimal */
-                 r = 16; break;
-
-             default:                    /* Unknown type (pass-through) */
-                 putc_bfd(&pb, c); continue;
-             }
-
-             /* Get an argument and put it in numeral */
-             v = (f & 4) ? (DWORD)va_arg(arp, long) : ((d == 'D') ? (DWORD)(long)va_arg(arp, int) : (DWORD)va_arg(arp, unsigned int));
-             if (d == 'D' && (v & 0x80000000)) {
-                 v = 0 - v;
-                 f |= 8;
-             }
-             i = 0;
-             do {
-                 d = (TCHAR)(v % r); v /= r;
-                 if (d > 9) d += (c == 'x') ? 0x27 : 0x07;
-                 str[i++] = d + '0';
-             } while (v && i < sizeof str / sizeof *str);
-             if (f & 8) str[i++] = '-';
-             j = i; d = (f & 1) ? '0' : ' ';
-             if (!(f & 2)) {
-                 while (j++ < w) putc_bfd(&pb, d);   /* Right pad */
-             }
-             do {
-                 putc_bfd(&pb, str[--i]);            /* Number body */
-             } while (i);
-             while (j++ < w) putc_bfd(&pb, d);       /* Left pad */
-         }
-
-         va_end(arp);
-
-         return putc_flush(&pb);
-#endif
-  }
-#endif
-  }
-
-  return (int)VfsError::INVAL;
+  size_t written;
+  this->write(line, strlen(line), written);
+  return written;
 }
 #endif
 
